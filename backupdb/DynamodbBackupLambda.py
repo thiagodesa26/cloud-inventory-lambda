ae162b0850a2009b8f3b9def21453de92b808cd0
Thiago de Sa <thiagodesa26@gmail.com>
2020-04-13 08:23:19 -0300

    chore: added cloudwatch logging

--- backupdb/DynamodbBackupLambda.py
+++ backupdb/DynamodbBackupLambda.py
@@ -1,9 +1,12 @@
 import boto3
 
 client = boto3.client('dynamodb')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
+    logger.info('Function:{} has started'.format(context.function_name))
     client.create_backup(
         TableName='inventory',
         BackupName='MyAutomaticBackup'
